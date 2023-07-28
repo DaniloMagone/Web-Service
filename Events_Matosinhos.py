fef0250a70b4b3b5c20eb09dab954499b93b5b8c
DaniloMagone <59653637+DaniloMagone@users.noreply.github.com>
2023-07-28 09:49:10 -0300

    Update Events_Matosinhos.py

--- Events_Matosinhos.py
+++ Events_Matosinhos.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from fuzzywuzzy import process, fuzz
 import numpy as np
-
+from Levenshtein import distance as lev
 def formata(palavra):
   #comando para letra minuscula
   palavra = palavra.lower()
@@ -29,8 +29,7 @@ host = "https://www.cm-matosinhos.pt/"
 link_list = []
 for j in range(0,195):
    link_list.append((host + '/servicos-municipais/comunicacao-e-imagem/eventos?events_list_37_page=' + str(j+1)))
-
-# aqui está um exemplo de uso +- igual o seu
+#Extrai os dados procurando pelas tags HTML onde estão os dados
 table = {i:[] for i in "titulo,link,dia_inicio,dia_final,mes_inicio,mes_final,ano_inicio,ano_final".split(",")}
 for link in link_list:
   link = requests.get(link)
@@ -41,6 +40,8 @@ for link in link_list:
     mes = [ x.text for x in post.find_all("span",class_="mes_curto")]
     ano = [ x.text for x in post.find_all("span",class_="ano")]
     for var in [dia,mes,ano] :
+      #caso não haja dois campos de dias/meses/anos, então considera-se que o dia, mês ou ano
+      # são os mesmos que os iniciais.
       if len(var) == 1 : var *= 2
     table["dia_inicio"].append(dia[0])
     table["dia_final"].append(dia[1])
@@ -48,13 +49,17 @@ for link in link_list:
     table["mes_final"].append(mes[1])
     table["ano_inicio"].append(ano[0])
     table["ano_final"].append(ano[1])
-    table["titulo"].append(host+post.find("a",class_="linl_overlay").get("href"))
-    table["link"].append(post.find("div",class_="title").find("h2").text)
+    table["link"].append(host+post.find("a",class_="linl_overlay").get("href"))
+    table["titulo"].append(post.find("div",class_="title").find("h2").text)
 df = pd.DataFrame(table)
-df["format"] = df["link"].apply(lambda x: formata(x))
+#Formata os títulos dos eventos para faciitar a identificação de eventos recorrentes
+df["format"] = df["titulo"].apply(lambda x: formata(x))
 df['ano_final'] = df['ano_final'].str.replace("'", "")
 df['ano_final'] = pd.to_numeric(df['ano_final'])
 df.drop(df[df['ano_final'] < 16].index, inplace = True)
+#Os eventos recorrentes são identificados pela semelhança entre os títulos
+# descartando pontuação, caractéres especiais, números romanos e espaços.
+# A semelhança é calculada utilizando o pacote Levenshtein juntamente com o FuzzyWuzzy
 unique_events = df['link'].unique().tolist()
 score_sort = [(x,) + i
              for x in unique_events
@@ -66,7 +71,6 @@ high_score_sort = similarity_sort[(similarity_sort['score_sort'] >= 71) &
                 (similarity_sort['event_sort'] !=  similarity_sort['match_sort']) &
                 (similarity_sort['sorted_event_sort'] != similarity_sort['match_sort'])]
 high_score_sort = high_score_sort.drop('sorted_event_sort',axis=1).copy()
-
 new_df = df
 new_df["match"]  = df["link"]
 for key, value in high_score_sort['event_sort'].iteritems():
